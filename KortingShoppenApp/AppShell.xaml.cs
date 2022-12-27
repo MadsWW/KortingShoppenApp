39c30b9c64ae953a40462cf04ff553fef8feb631
MadsWW <33266830+MadsWW@users.noreply.github.com>
2022-12-27 16:30:40 +0100

    Add project files.

---
+++ KortingShoppenApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KortingShoppenApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ KortingShoppenApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KortingShoppenApp;
+
+public partial class MainPage : TabbedPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
